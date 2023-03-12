4f995e64f2f140bea26b064171dd31b09a102768
Phillip Marques <phillipmarq@hotmail.com>
2023-03-11 22:03:06 -0300

    Getting user by ID

--- src/main/java/com/pms/mongo/controller/UserController.java
+++ src/main/java/com/pms/mongo/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,11 @@ public class UserController {
 		List<User> user = userService.findAll();
 		List<UserDTO> userDTO = user.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(userDTO);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/com/pms/mongo/service/UserService.java
+++ src/main/java/com/pms/mongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.pms.mongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pms.mongo.entity.User;
 import com.pms.mongo.repository.UserRepository;
+import com.pms.mongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,10 @@ public class UserService {
 	public List<User> findAll(){
 		return userRepository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+			
 }
