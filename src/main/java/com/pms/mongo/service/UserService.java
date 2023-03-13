d24ed44982de58242db0cf2fd2bd26e6fa245a46
Phillip Marques <phillipmarq@hotmail.com>
2023-03-13 07:47:38 -0300

    User update with PUT

--- src/main/java/com/pms/mongo/service/UserService.java
+++ src/main/java/com/pms/mongo/service/UserService.java
@@ -13,29 +13,39 @@ import com.pms.mongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
-	
-	
+
 	public User insert(User user) {
 		return userRepository.insert(user);
 	}
+
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		return userRepository.save(newUser);
+	}
 	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
 	}
-	
+
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
