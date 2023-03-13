4b37850c13f825b2e3cd97617aa0ef563bc6e31d
Phillip Marques <phillipmarq@hotmail.com>
2023-03-13 08:35:04 -0300

    Author data projection with DTO

--- src/main/java/com/pms/mongo/config/Instantiaiton.java
+++ src/main/java/com/pms/mongo/config/Instantiaiton.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.pms.mongo.dto.AuthorDTO;
 import com.pms.mongo.entity.Post;
 import com.pms.mongo.entity.User;
 import com.pms.mongo.repository.PostRepository;
@@ -37,8 +38,8 @@ public class Instantiaiton implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 	
 		postRepository.saveAll(Arrays.asList(post1, post2));	
 	
